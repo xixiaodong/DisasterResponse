703d425d62c159af61f5bab7b399431481bcb3d1
XiXiaodong <s1508628@sms.ed.ac.uk>
2020-09-21 12:21:52 +0100

    added clean_data function

--- data/process_data.py
+++ data/process_data.py
@@ -23,8 +23,43 @@ def load_data(messages_filepath, categories_filepath):
     return df
 
 def clean_data(df):
-    
+    '''
+    clean data function
 
+    Args:
+      df : pandas DataFrame containing raw data
+    
+    Returns:
+      df: cleaned dataset in pandas DataFrame
+    '''
+
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories.str.split(pat=';',expand=True)
+    
+    # select the first row of the categories dataframe
+    row1 = categories.iloc[0,:]
+    
+    # use this row to extract a list of new column names for categories
+    category_colnames = row1.apply(lambda x:x[:-2])
+    
+    # rename the columns of 'categories'
+    categories.columns = category_colnames
+    
+    # convert category values to just numbers 0 or 1
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(np.int)
+
+    # drop the original categories column from 'df'
+    df = df.drop('categories',axis=1)
+
+    #concatenate the original dataframe with the new 'categories' dataframe
+    df = pd.concat([df,categories],axis=1)
+    
+    # remove duplicates
+    df = df.drop_duplicates()
+    
+    return df
 
 def save_data(df, database_filename):
     pass  

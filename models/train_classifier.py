6d5f6f5cd623d1fa3182c339a6d7b8932799ad8e
XiXiaodong <s1508628@sms.ed.ac.uk>
2020-09-21 15:05:02 +0100

    Updated functions in train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,25 +1,148 @@
+'''
+This is part of the Disaster Response project for Udacity Data
+Science Nanodegree.
+
+This script contains the Machine Learning (ML) pipeline.
+The pipeline consists of functions to load the data, tokenise 
+the texts, build predictive model, evaluate model performance, 
+and save the model artefact. 
+
+Author: Xiaodong Xi
+'''
+
+# load libraries
 import sys
+import pandas as pd
+import numpy as np
+import os
+import pickle
+from sqlalchemy import create_engine
+import re
+import nltk
+
+from scipy.stats import hmean
+from scipy.stats.mstats import gmean
+
+from sklearn.model_selection import train_test_split
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier,AdaBoostClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import make_scorer, accuracy_score, f1_score, fbeta_score, classification_report
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline, FeatureUnion
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 
 
 def load_data(database_filepath):
-    pass
+    '''
+    load data function
 
+    Args:
+        database_filepath : location of database (.db) file
+
+    Returns:
+        X : feature matrix in DataFrame
+        Y : target vector in DataFrame
+        category_names : name of categories (pandas index)
+    '''
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('df',engine)
+    X = df['message']
+    Y = df.iloc[:,4:]
+    category_names = Y.columns
+    
+    return X, Y, category_names
 
 def tokenize(text):
-    pass
+    '''
+    tokenise function
+
+    Args:
+        text : text messages (list)
+
+    Returns:
+        clean_tokens : cleaned tokenised text (list)
+    '''
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    '''
+    build model function
+
+    Args: 
+        None
+    
+    Returns:
+        model : model object
+    '''
+    pipeline = Pipeline([
+                        ('vect', CountVectorizer(tokenizer=tokenize)),
+                        ('tfidf', TfidfTransformer()),
+                        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+                        ])
+
+    parameters = {'clf__estimator__n_estimators': [50, 100],
+                  'clf__estimator__min_samples_split': [2, 3, 4],
+                  'clf__estimator__criterion': ['entropy', 'gini']
+                 }
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    
+    return cv
+
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    evaluate model function
+
+    Args:
+        model : model object
+        X_test : testing feature matrix (DataFrame)
+        Y_test : testing target vector (DataFrame)
+        category_names : 
+    '''
+    Y_pred = model.predict(X_test)
+    
+    # Calculate the accuracy for each of them.
+    for j in range(len(category_names)):
+        print("Category:", category_names[j],"\n", classification_report(Y_test.iloc[:, j].values, Y_pred[:, j]))
+        print('Accuracy of %25s: %.2f' %(category_names[j], accuracy_score(Y_test.iloc[:, j].values, Y_pred[:, j])))
+
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    save model function
 
+    Args:
+        model : model object
+        model_filepath: location to store model
+    Returns:
+        None (performs saving model action)
+    '''
+
+    pickle.dump(model, open(model_filepath, "wb"))
+
+    pass
 
 def main():
     if len(sys.argv) == 3:
